aafb8e8a9e71429a3ff562ff33e7c6eb8cddaa83
bingzhe wu <1200010666@pku.edu.cn>
2015-04-17 23:31:13 +0800

    homework 3.4

--- 数值分析/homework3_2.cpp
+++ 数值分析/homework3_2.cpp
@@ -15,23 +15,21 @@ double f2(double x0,double x1,double y0,double y1){
 void output4_2(double a,double b,int maxNum){
     cout.precision(7);
     cout<<"h&"<<" $\\pi$& "<<"复合中点&"<<" 误差"<<"\\\\"<<endl;
-    for(int n=0;n<maxNum;n=n+10){
-        int n0=100*n;
-        double h=(b-a)/double(n0);
+    for(int n=1;n<maxNum;n=n*10){
+        double h=(b-a)/double(n);
         double center=re_mide_point(a,b,h,f);
         cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
     }
     cout<<"h&"<<" $\\pi$& "<<"复合梯形&"<<" 误差"<<"\\\\"<<endl;
-    for(int n=0;n<maxNum;n=n+10){
-        int n0=100*n;
-        double h=(b-a)/double(n0);
+    for(int n=1;n<maxNum;n=n*10){
+        double h=(b-a)/double(n);
         double center=re_trapezoid(a,b,h,f);
         cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
     }
     cout<<"h&"<<" $\\pi$& "<<"复合Simposon&"<<" 误差"<<"\\\\"<<endl;
-    for(int n=0;n<maxNum;n=n+10){
-        int n0=100*n;
-        double h=(b-a)/double(n0);
+    for(int n=1;n<maxNum;n=n*10){
+       
+        double h=(b-a)/double(n);
         double center=re_simposon(a,b,h,f);
         cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
     }
@@ -42,7 +40,7 @@ int main()
 {
     //3.4
     double a=0.0,b=1.0;
-    output4_2(a,b,100);
+    output4_2(a,b,10000000);
     //3.9
     //double a1=-1,b1=1,c1=-1,d1=1;
    // std::cout<<double_simposon(a1,b1,c1,d1,2.0,2.0,f2)<<std::endl;
