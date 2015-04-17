06c81bb2abe67df25afa7223d6c0f0ad26f5df04
bingzhe wu <1200010666@pku.edu.cn>
2015-04-17 22:27:24 +0800

    homework 3.4

--- 数值分析/homework3_2.cpp
+++ 数值分析/homework3_2.cpp
@@ -3,6 +3,7 @@
 #include<iomanip>
 #include"integration.h"
 using namespace std;
+#define Pi 3.141592653589793
 double f(double x)
 {
     return 4.0/(1+x*x);
@@ -11,15 +12,39 @@ double f2(double x0,double x1,double y0,double y1){
     return 1.0/std::sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0));
 
 }
+void output4_2(double a,double b,int maxNum){
+    cout.precision(7);
+    cout<<"h&"<<" $\\pi$& "<<"复合中点&"<<" 误差"<<"\\\\"<<endl;
+    for(int n=0;n<maxNum;n=n+10){
+        int n0=100*n;
+        double h=(b-a)/double(n0);
+        double center=re_mide_point(a,b,h,f);
+        cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
+    }
+    cout<<"h&"<<" $\\pi$& "<<"复合梯形&"<<" 误差"<<"\\\\"<<endl;
+    for(int n=0;n<maxNum;n=n+10){
+        int n0=100*n;
+        double h=(b-a)/double(n0);
+        double center=re_trapezoid(a,b,h,f);
+        cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
+    }
+    cout<<"h&"<<" $\\pi$& "<<"复合Simposon&"<<" 误差"<<"\\\\"<<endl;
+    for(int n=0;n<maxNum;n=n+10){
+        int n0=100*n;
+        double h=(b-a)/double(n0);
+        double center=re_simposon(a,b,h,f);
+        cout<<h<<"& "<<Pi<<"& "<<center<<"& "<<abs(center-Pi)<<"\\\\"<<endl;
+    }
+
+}
+
 int main()
 {
     //3.4
     double a=0.0,b=1.0;
-    double h=0.00001;double err=0.0000001;int n0=(b-a)/h;
-    std::cout<<"中点 "<<re_mide_point(a,b,h,f)<<"梯形 "<<
-        re_trapezoid(a,b,h,f)<<"simposon: "<<re_simposon(a,b,h,f)<<std::endl;
-    cout<<"romberg"<<setprecision(8)<<romberg(a,b,h,f,err)<<"自适应方法 "<<auto_fit(a,b,n0,f,err)<<std::endl;
+    output4_2(a,b,100);
     //3.9
-    double a1=-1,b1=1,c1=-1,d1=1;
-    std::cout<<double_simposon(a1,b1,c1,d1,2.0,2.0,f2)<<std::endl;
+    //double a1=-1,b1=1,c1=-1,d1=1;
+   // std::cout<<double_simposon(a1,b1,c1,d1,2.0,2.0,f2)<<std::endl;
+
 }
