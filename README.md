da7a6e0aefb63c43cae64cdb8bfe9ed3bf8e3ae8
root <root@iZ257br7qebZ.(none)>
2015-05-12 17:48:59 +0800

    write a *.py for test scipy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Github
 
-Hi my name is Bingzhe 
+Hi. My name is Bingzhe 
 
 This file include some codes and docs taht I was learing
 
--- learn_Python/learn_numpy.py
+++ learn_Python/learn_numpy.py
@@ -1,2 +1,10 @@
 import numpy
-import scipy
+from scipy.optimize import fsolve 
+import math
+
+def f1(x):
+	return math.cos(x)+1.0/(1+math.exp(-2*x))
+if __name__=='__main__':
+	result=fsolve(f1,3.0)
+	print result
+
