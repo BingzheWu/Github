e83d226f042338afd4836c0000b0ce7c5c9337de
bingzhe wu <1200010666@pku.edu.cn>
2015-05-12 16:24:44 +0800

    delete some file

--- numrical/Complex.h
+++
@@ -1,4 +0,0 @@
-#ifndef _Complex__H_
-#define _Complex__H_
-class complex
-#endif
--- numrical/normal.py
+++
@@ -1,8 +0,0 @@
-import re
-import os
-def normal(fn):
-    r=open(fn,'r')
-    for line in r:
-        if 'service_id' in line:
-            continue
-        
