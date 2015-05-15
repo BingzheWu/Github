aba7faac86c314b44fee02dd8b25f3fc229a394d
1200010666@pku.edu.cn <1200010666@pku.edu.cn>
2015-05-15 11:46:45 +0800

    complet

--- learn_Python/learn_numpy.py
+++ learn_Python/learn_numpy.py
@@ -1,10 +1,12 @@
 import numpy
 from scipy.optimize import fsolve 
-import math
+from math import sin,cos
 
 def f1(x):
 	return math.cos(x)+1.0/(1+math.exp(-2*x))
+def f2(x):
+    return [-cos(x[0])/81.0+x[1]*x[1]/9.0+sin(x[2])/3.0-x[0],sin(x[0])/3.0+cos(x[2])/3.0-x[1],-cos(x[0])/9.0+x[1]/3.0+sin(x[2])/6.0-x[2]]
 if __name__=='__main__':
-	result=fsolve(f1,3.0)
+	result=fsolve(f2,[1,1,-1])
 	print result
 
