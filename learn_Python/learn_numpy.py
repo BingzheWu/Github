c589b4fb080c7edd30ad1c00c6ca3f3345c14096
wubingzhe <wubingzhe@vm10-136-0-120.ksc.com>
2015-05-29 11:16:09 +0800

    add spider

--- learn_Python/learn_numpy.py
+++ learn_Python/learn_numpy.py
@@ -1,9 +1,9 @@
 import numpy
 from scipy.optimize import fsolve 
-from math import sin,cos
+from math import sin,cos,exp
 
 def f1(x):
-    return math.cos(x)+1.0/(1+math.exp(-2*x))
+    return cos(x)+1.0/(1+exp(-2*x))
 def f2(x):
     return [-cos(x[0])/81.0+x[1]*x[1]/9.0+sin(x[2])/3.0-x[0],sin(x[0])/3.0+cos(x[2])/3.0-x[1],-cos(x[0])/9.0+x[1]/3.0+sin(x[2])/6.0-x[2]]
 if __name__=='__main__':
