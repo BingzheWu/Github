5461fa64ec494c5bfcf7c8d6e70cb18e2e0953fb
1200010666@pku.edu.cn <1200010666@pku.edu.cn>
2015-06-01 10:54:41 +0800

    plot fft time

---
+++ learn_Python/MatrixFFT.py
@@ -0,0 +1,28 @@
+# --coding--=utf8
+from pylab import *
+import math
+import sys
+reload(sys)
+def initData(r):
+    FFTtime=[]
+    PCGtime=[]
+    size=[]
+    for Info in r:
+        Info=Info.split(',')
+        size.append(float(Info[0]))
+        FFTtime.append(float(Info[1]))
+        PCGtime.append(float(Info[2]))
+    return size,FFTtime,PCGtime
+def plotTime(r):
+    size,FFTtime,PCGtime=initData(r)
+    plot(size,FFTtime,'r',label=u'FFT')
+    plot(size,PCGtime,'b',label=u'PCG')
+    legend()
+    xlabel('MatrixSize')
+    ylabel('time')
+    show()
+if __name__=='__main__':
+    fileName=sys.argv[1]
+    r=open(fileName,'r')
+    plotTime(r)
+
